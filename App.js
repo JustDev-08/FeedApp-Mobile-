daba300754d10614aaa5324a12f25707510d2fa7
JustDev-08 <justdev08@gmail.com>
2021-12-19 21:00:01 +0700

    First Commit

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+// import Page
+import FormPage from './src/form.js'
+import CheckPage from './src/check.js'
+// As Tab
+const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+     <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+  tabBarIcon: ({ focused, color, size }) => {
+    let iconName;
+
+    if (route.name === 'Feed') {
+      iconName = focused
+        ? 'paw-sharp'
+        : 'paw-outline';
+    } else if (route.name === 'Check') {
+      iconName = focused ? 'eye-sharp' : 'eye-outline';
+    }
+
+    // You can return any component that you like here!
+    return <Ionicons name={iconName} size={size} color={color} />;
+  },
+  tabBarActiveTintColor: 'tomato',
+  tabBarInactiveTintColor: 'gray',
+})}
+>
+        <Tab.Screen name="Feed" component={FormPage} />
+        <Tab.Screen name="Check" component={CheckPage} logo />
+      </Tab.Navigator>
+    </NavigationContainer>
+
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/check.js
@@ -0,0 +1,11 @@
+import {View , Text } from 'react-native'
+
+function CheckPage() {
+	return (
+	<View>
+		<Text>This is CheckPage</Text>
+	</View> 
+	)
+}
+
+export default CheckPage
